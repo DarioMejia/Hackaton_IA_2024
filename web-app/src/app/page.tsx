74777b2aa483b22e16c3f39dab6293414f13f3ed
Daniel Pedroza <teknaive@gmail.com>
2024-07-29 23:33:44 -0500

    Último Commit de la rama
    Último Commit de la rama para terminar de adornarla.

--- web-app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-    return (
-        <div className="container">
-        <h1>ProcessOptima</h1>
-        <p>Proyecto de Hackaton IA Julio 2024</p>
-        </div>
-    );
-}
